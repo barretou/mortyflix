267eabf40f4b40cf06f8d57a2f94a3e00d5e07c2
barreto <gustavo.barreto@aqtech.com>
2022-12-10 16:01:34 -0300

    Fetch logic an pagination o characters view

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 const baseURL = 'https://rickandmortyapi.com/api';
 
-async function GetCharacters() {
-  const resp = await fetch(`${baseURL}/character`)
+async function GetCharacters(page) {
+  const resp = await fetch(`${baseURL}/character/?page=${page}`)
     .then((response) => response.json())
     .catch((error) => console.log(error));
   return resp;
--- src/views/CharactersView.vue
+++ src/views/CharactersView.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="container">
+  <section class="container">
     <div class="row">
-      <div class="col-12">
-        <h1 class="my-3">Personagens</h1>
+      <h1 class="my-3">Personagens</h1>
+      <div class="col-12 d-flex justify-content-between flex-wrap">
+        <div
+          v-for="item in characters.results"
+          :key="item.id"
+          class="card m-2"
+          style="width: 18rem"
+        >
+          <img :src="item.image" class="card-img-top" :alt="item.description" />
+          <div class="card-body">
+            <h5 class="card-title">{{ item.name }}</h5>
+            <p class="card-text">
+              <span>Status:</span>
+              {{ item.status }}
+            </p>
+            <p class="card-text">
+              <span>Specie:</span>
+              {{ item.species }}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-content-end">
+        <button class="btn btn-info m-1 d-block" @click="DecrementCurrentPage()">Previous</button>
+        <button class="btn btn-info m-1 d-block" @click="IncrementCurrentPage()">Next</button>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -15,11 +38,31 @@ export default {
   data() {
     return {
       characters: [],
+      currentPage: 1,
     };
   },
   async mounted() {
-    this.characters = await GetCharacters();
-    console.log(this.characters);
+    this.characters = await GetCharacters(this.currentPage);
+  },
+  methods: {
+    async IncrementCurrentPage() {
+      this.currentPage += 1;
+      this.characters = await GetCharacters(this.currentPage);
+    },
+    async DecrementCurrentPage() {
+      if (this.currentPage >= 2) {
+        this.currentPage -= 1;
+        this.characters = await GetCharacters(this.currentPage);
+      }
+    },
   },
 };
 </script>
+
+<style scoped>
+h5,
+h1,
+span {
+  color: #2c3e50;
+}
+</style>
